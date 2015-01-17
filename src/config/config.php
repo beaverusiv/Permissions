9dfb99adc5d87f8e17e3969e2bebede0c65e1b38
Nic Loomans <nic@bocapa.com>
2015-01-17 19:09:53 +1300

    Added config file

---
+++ src/config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+     * Group IDs are in the config because it is much quicker to access
+     * the groups if the ID is known. Generally these will not need to
+     * be changed.
+     */
+
+    'default_id' => 1,
+
+    'registered_id' => 2,
+
+    'admin_id' => 3
+];
