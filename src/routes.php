316085ac294ec2634dc0fc4c56e7e72a0f226109
Nic Loomans <nic@bocapa.com>
2015-01-17 14:14:09 +1300

    Removed view route. Not needed when we have an edit route

--- src/routes.php
+++ src/routes.php
@@ -4,4 +4,3 @@ Route::get('admin/groups/browse', ['as' => 'groups.adminBrowse', 'uses' => 'Grou
 Route::get('admin/groups/edit/{id}', ['as' => 'groups.adminEdit', 'uses' => 'GroupsController@adminEdit'])->where('id', '[0-9]+');
 Route::post('admin/groups/edit/{id}', ['as' => 'groups.adminSave', 'uses' => 'GroupsController@adminSave'])->where('id', '[0-9]+');
 Route::post('admin/groups/delete/{id}', ['as' => 'groups.adminDelete', 'uses' => 'GroupsController@adminDelete'])->where('id', '[0-9]+');
-Route::get('admin/groups/{id}', ['as' => 'groups.adminView', 'uses' => 'GroupsController@adminView'])->where('id', '[0-9]+');
