1491e69a18f5bdbbc1fbed776abe85f758022cca
Nic Loomans <nic@bocapa.com>
2015-02-11 11:38:24 +1300

    Namespaced routes

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::any('admin/groups/browse', ['as' => 'groups.adminBrowse', 'uses' => 'GroupsController@adminBrowse']);
-Route::get('admin/groups/edit/{id}', ['as' => 'groups.adminEdit', 'uses' => 'GroupsController@adminEdit'])->where('id', '[0-9]+');
-Route::post('admin/groups/edit/{id}', ['as' => 'groups.adminSave', 'uses' => 'GroupsController@adminSave'])->where('id', '[0-9]+');
-Route::get('admin/groups/delete/{id}', ['as' => 'groups.adminDelete', 'uses' => 'GroupsController@adminDelete'])->where('id', '[0-9]+');
+Route::any('admin/groups/browse', ['as' => 'groups.adminBrowse', 'uses' => 'Bocapa\Permissions\Controllers\GroupsController@adminBrowse']);
+Route::get('admin/groups/edit/{id}', ['as' => 'groups.adminEdit', 'uses' => 'Bocapa\Permissions\Controllers\GroupsController@adminEdit'])->where('id', '[0-9]+');
+Route::post('admin/groups/edit/{id}', ['as' => 'groups.adminSave', 'uses' => 'Bocapa\Permissions\Controllers\GroupsController@adminSave'])->where('id', '[0-9]+');
+Route::get('admin/groups/delete/{id}', ['as' => 'groups.adminDelete', 'uses' => 'Bocapa\Permissions\Controllers\GroupsController@adminDelete'])->where('id', '[0-9]+');
