8c9b2e7feb3e9a824c42aa330c163083db18491d
Nic Loomans <nic@bocapa.com>
2015-02-11 11:43:02 +1300

    Escaped Facades in controller

--- src/controllers/GroupsController.php
+++ src/controllers/GroupsController.php
@@ -7,25 +7,25 @@ class GroupsController extends Controller {
 
     public function adminBrowse()
     {
-        if(Request::wantsJson()) {
+        if(\Request::wantsJson()) {
             return Group::all()->toJson();
         }
 
-        return View::make('permissions::groups/admin_browse')
-                ->with(['groups' => Group::all(), 'user' => Auth::user()]);
+        return \View::make('permissions::groups/admin_browse')
+                ->with(['groups' => Group::all(), 'user' => \Auth::user()]);
     }
 
     public function adminEdit($id)
     {
         if(0 == $id) {
-            return View::make('permissions::groups/admin_edit');
+            return \View::make('permissions::groups/admin_edit');
         }
 
         $group = Group::findOrFail($id);
 
-        Event::fire('permissions.generate', [$group]);
+        \Event::fire('permissions.generate', [$group]);
 
-        return View::make('permissions::groups/admin_edit')
+        return \View::make('permissions::groups/admin_edit')
                 ->with(compact('group'));
     }
 
@@ -37,28 +37,28 @@ class GroupsController extends Controller {
             $group = Group::findOrFail($id);
         }
         // If name was edited
-        if(Input::has('name')) {
-            $group->fill(Input::all())->save();
+        if(\Input::has('name')) {
+            $group->fill(\Input::all())->save();
             $id = $group->id;
 
         // Otherwise it was the permissions that were edited
         } else {
             foreach($group->permissions()->get() as $permission) {
-                $permission->name = Input::get($permission->id.'_name', '');
-                $permission->permitted = Input::get($permission->id.'_permitted', false);
+                $permission->name = \Input::get($permission->id.'_name', '');
+                $permission->permitted = \Input::get($permission->id.'_permitted', false);
                 $permission->save();
             }
 
-            Event::fire('permissions.changed');
+            \Event::fire('permissions.changed');
         }
 
-        return Redirect::route('groups.adminEdit', ['id' => $id]);
+        return \Redirect::route('groups.adminEdit', ['id' => $id]);
     }
 
     public function adminDelete($id)
     {
         Group::destroy($id);
 
-        return Redirect::route('groups.adminBrowse');
+        return \Redirect::route('groups.adminBrowse');
     }
 }
